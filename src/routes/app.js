4837cb095a8e92a8367f5f6da3dbb98e738665d0
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-05-23 23:47:40 +0100

    Create app.js

---
+++ src/routes/app.js
@@ -0,0 +1,31 @@
+//* BLOGS APPLICATION
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const cookie = require("cookie-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.options("*", cors());
+app.use(helmet());
+app.use(cookie());
+
+const User_Router = require("../routes/user");
+const BlogPost_Router = require("../routes/blogPost");
+const Admins_Router = require("../routes/admins");
+const SuperAdmins_Router = require("../routes/superAdmins");
+
+app.use("/api/user/", User_Router);
+app.use("/api/blog/", BlogPost_Router);
+// app.use("/api/admin/", Admins_Router);
+// app.use("/api/superAdmin/", SuperAdmins_Router);
+app.get("/api/home", (req, res) => {
+  res.status(200).send("welcome to the home page");
+});
+
+module.exports = app;
