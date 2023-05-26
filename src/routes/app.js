214739962f237bbb3c0543e1cd71aedc2d258968
OtiDavidChibueze <109368512+OtiDavidChibueze@users.noreply.github.com>
2023-05-27 00:10:26 +0100

    Update app.js

--- src/routes/app.js
+++ src/routes/app.js
@@ -19,11 +19,13 @@ const User_Router = require("../routes/user");
 const BlogPost_Router = require("../routes/blogPost");
 const Admins_Router = require("../routes/admins");
 const SuperAdmins_Router = require("../routes/superAdmins");
+const comment_Router = require("../routes/comments");
 
 app.use("/api/user/", User_Router);
 app.use("/api/blog/", BlogPost_Router);
 app.use("/api/admin/", Admins_Router);
 app.use("/api/superAdmin/", SuperAdmins_Router);
+app.use("/api/comment/", comment_Router);
 app.get("/api/home", (req, res) => {
   res.status(200).send("welcome to the home page");
 });
